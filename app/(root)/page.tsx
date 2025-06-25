f3745ede33dcb66521c92ecc19b05747e6bcfee1
WMC <18013550018@163.com>
2025-06-25 17:40:26 +0800

    feat: Fetching Data and Type Safety

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
 import SearchForm from '@/components/SearchForm';
-import StartupCard from '@/components/StartupCard';
+import StartupCard, { StartupTypeCard } from '@/components/StartupCard';
+import { client } from '@/sanity/lib/client';
+import { STARTUPS_QUERY } from '@/sanity/lib/queries';
 
 export default async function Home({
 	searchParams,
@@ -8,19 +10,8 @@ export default async function Home({
 }) {
 	const query = (await searchParams).query;
 
-	const posts = [
-		{
-			createdAt: new Date(),
-			views: 55,
-			author: { _id: 1, name: 'Adrian' },
-			_id: 1,
-			description: 'This is a description.',
-			image:
-				'https://images.unsplash.com/photo-1634912314704-c646c586b131?9=808w=29408auto=format&fit=cropsixlib=rb4.0,38ixid=M3wxMiA3fDB8MHxwaG90bv1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-			category: 'Robots',
-			title: 'We Robots',
-		},
-	];
+	const posts = await client.fetch(STARTUPS_QUERY);
+
 	return (
 		<>
 			<section className="pink_container">
@@ -43,7 +34,9 @@ export default async function Home({
 				</p>
 				<ul className="mt-7 card_grid">
 					{posts?.length > 0 ? (
-						posts.map((post) => <StartupCard key={post?._id} post={post} />)
+						posts.map((post: StartupTypeCard) => (
+							<StartupCard key={post?._id} post={post} />
+						))
 					) : (
 						<p className="no-results">No startups found</p>
 					)}
--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,21 +4,15 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { Button } from './ui/button';
+import { Startup, Author } from '@/sanity/types';
+
+export type StartupTypeCard = Omit<Startup, 'author'> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
-	// createdAt: new Date(),
-	// views: 55,
-	// author: { _id: 1, name: 'Adrian' },
-	// _id: 1,
-	// description: 'This is a description.',
-	// image:
-	// 	'https://images.unsplash.com/photo-1634912314704-c646c586b131?9=808w=29408auto=format&fit=cropsixlib=rb4.0,38ixid=M3wxMiA3fDB8MHxwaG90bv1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-	// category: 'Robots',
-	// title: 'We Robots',
 	const {
-		createdAt,
+		_createdAt,
 		views,
-		author: { _id: authorId, name },
+		author,
 		_id,
 		description,
 		image,
@@ -28,7 +22,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 	return (
 		<li className="startup-card group">
 			<div className="flex-between">
-				<p className="startup_card_date">{formatDate(createdAt)}</p>
+				<p className="startup_card_date">{formatDate(_createdAt)}</p>
 				<div className="flex gap-1.5">
 					<EyeIcon className="size-6 text-primary" />
 					<span className="text-16-medium">{views}</span>
@@ -37,17 +31,17 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
 			<div className="flex-between mt-5 gap-5">
 				<div className="flex-1">
-					<Link href={`/user/${authorId}`}>
-						<p className="text-16-medium line-clamp-1">{name}</p>
+					<Link href={`/user/${author?._id}`}>
+						<p className="text-16-medium line-clamp-1">{author?.name}</p>
 					</Link>
 					<Link href={`/startup/${_id}`}>
 						<h3 className="text-26-semibold line-clamp-1">{title}</h3>
 					</Link>
 				</div>
-				<Link href={`/user/${authorId}`}>
+				<Link href={`/user/${author?._id}`}>
 					<Image
 						src="https://placehold.co/48x48"
-						alt={title}
+						alt="placeholder"
 						width={48}
 						height={48}
 						className="rounded-full"
@@ -61,7 +55,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 			</Link>
 
 			<div className="flex-between gap-3 mt-5">
-				<Link href={`/?query=${category.toLowerCase()}`}>
+				<Link href={`/?query=${category?.toLowerCase()}`}>
 					<p className="text-16-medium">{category}</p>
 				</Link>
 				<Button className="startup-card_btn" asChild>
